1a75a96fff1a5122e777e9fc713a2851860230dc
RedBlueGame <158830320+RedBlueGame@users.noreply.github.com>
2025-11-30 15:53:56 +0300

    DyingOfTail

--- Snake/Assets/Script/SnakeTail.cs
+++ Snake/Assets/Script/SnakeTail.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class SnakeTail : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        Rigidbody2D rb = collision.attachedRigidbody;
+        if (rb != null)
+        {
+            SnakeHead snakeHead = rb.GetComponent<SnakeHead>();
+            if (snakeHead != null)
+            {
+                //snakeHead.Die();
+            }
+        }
     }
 }
